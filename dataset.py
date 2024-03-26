cda8db082466e3b67c29b5189a92bda389ca3b1c
Esme <esmepuzio@ucsb.edu>
2024-03-26 10:12:41 -0700

    Cleared old folders

--- dataset.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf 
-raw_dataset = tf.data.TFRecordDataset("output.tfrecord")
-
-for raw_record in raw_dataset.take(1):
-    example = tf.train.Example()
-    example.ParseFromString(raw_record.numpy())
-    print(example)
-    
